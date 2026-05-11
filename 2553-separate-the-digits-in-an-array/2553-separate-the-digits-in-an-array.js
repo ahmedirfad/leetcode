85a995e35998f48ef9e219d767a3c566a8efa96e
Ahmed Irfad <ahmedirfad8@gmail.com>
2026-05-11 09:54:13 +0530

    Time: 7 ms (32.22%), Space: 58.8 MB (65.56%) - LeetHub

---
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var separateDigits = function(nums) {
+    let result = [];
+
+    for(let num of nums){
+        let digits = num.toString();
+
+    for(let digit of digits){
+        result.push(Number(digit))
+    }
+    }
+    return result;
+};
